36ad64d7d211555ccd29573ef70d71b9d1a65475
Mahalia Bobbi <mabobbi@si-c8.nsk.21-school.ru>
2022-07-07 15:46:19 +0700

    change

--- simple_mc/function.c
+++ simple_mc/function.c
@@ -1,10 +1,13 @@
 #include "function.h"
 
 void printLeftWindow(WINDOW *left_win, struct dirent **data, int highlight, int n) {
-    int x = 2, y = 1;
-
+    int x = 2, y = 2;
     box(left_win, 0, 0);
     struct stat st;
+
+    mvwprintw(left_win, 1, 1, "Dir");
+    mvwprintw(left_win, 1, (COLS / 2) - 24, "Size");
+    mvwprintw(left_win, 1, (COLS / 2) - 14, "Modify time");
 
     wbkgd(left_win, COLOR_PAIR(1));
 
@@ -33,11 +36,15 @@ void printLeftWindow(WINDOW *left_win, struct dirent **data, int highlight, int 
         free(time);
         ++y;
     }
+    wscrl(left_win, y);
     wrefresh(left_win);
 }
 
 void printRightWindow(WINDOW *right_win, struct dirent **data, int highlight, int n) {
-    int x = 2, y = 1;
+    int x = 2, y = 2;
+    mvwprintw(right_win, 1, 1, "Dir");
+    mvwprintw(right_win, 1, (COLS / 2) - 24, "Size");
+    mvwprintw(right_win, 1, (COLS / 2) - 14, "Modify time");
 
     box(right_win, 0, 0);
     struct stat st;
@@ -78,4 +85,158 @@ char *printTime(struct stat *st) {
     char *tmp = (char *)malloc(sizeof(f));
     strcpy(tmp, f);
     return tmp;
+}
+
+void mcRun() {
+    WINDOW *left_win, *right_win;
+    int c = 0, flagWindow = 0;
+
+    initscr();
+
+    cbreak();
+    noecho();
+    curs_set(0);
+
+    start_color();
+    init_pair(1, COLOR_WHITE, COLOR_BLUE);
+    init_pair(2, COLOR_YELLOW, COLOR_BLUE);
+
+    right_win = newwin(LINES, COLS / 2, 0, COLS / 2);
+    left_win = newwin(LINES, COLS / 2, 0, 0);
+
+    char *curentDir = getcwd(NULL, 0);
+
+    struct dirent **leftNamelist;
+    struct dirent **rightNamelist;
+
+    int countLeftDir = scandir(curentDir, &leftNamelist, NULL, alphasort);
+    int countRightDir = scandir(curentDir, &rightNamelist, NULL, alphasort);
+
+    int highlightLeft = 0, highlightRight = 0;
+    highlightLeft = countLeftDir;
+    highlightRight = countRightDir;
+
+    keypad(left_win, TRUE);
+    keypad(right_win, TRUE);
+
+    refresh();
+
+    printLeftWindow(left_win, leftNamelist, highlightLeft, countLeftDir);
+    printRightWindow(right_win, rightNamelist, highlightRight, countRightDir);
+
+    while (c != 'q') {
+        if (flagWindow == 0) {
+            c = wgetch(left_win);
+        } else {
+            c = wgetch(right_win);
+        }
+        switch (c) {
+            case KEY_DOWN:
+                if (flagWindow == 0) {
+                    if (highlightLeft == 1) {
+                        highlightLeft = countLeftDir;
+                    } else {
+                        --highlightLeft;
+                    }
+                } else {
+                    if (highlightRight == 1) {
+                        highlightRight = countRightDir;
+                    } else {
+                        --highlightRight;
+                    }
+                }
+                break;
+            case KEY_UP:
+                if (flagWindow == 0) {
+                    if (highlightLeft == countLeftDir) {
+                        highlightLeft = 1;
+                    } else {
+                        ++highlightLeft;
+                    }
+                } else {
+                    if (highlightRight == countRightDir) {
+                        highlightRight = 1;
+                    } else {
+                        ++highlightRight;
+                    }
+                }
+                break;
+            case KEY_RIGHT:
+                flagWindow = 1;
+                break;
+            case KEY_LEFT:
+                flagWindow = 0;
+                break;
+            case 10:
+                if (flagWindow == 0) {
+                    wclear(left_win);
+
+                    char *leftBuff = getcwd(NULL, 0);
+                    strcat(leftBuff, "/");
+                    strcat(leftBuff, leftNamelist[highlightLeft - 1]->d_name);
+
+                    if (chdir(leftBuff) == 0) {
+                        while (countLeftDir--) {
+                            free(leftNamelist[countLeftDir]);
+                        }
+                        free(leftNamelist);
+
+                        countLeftDir = scandir(leftBuff, &leftNamelist, NULL, alphasort);
+                        highlightLeft = countLeftDir;
+
+                        free(leftBuff);
+                    } else {
+                        free(leftBuff);
+                    }
+                    break;
+                } else {
+                    wclear(right_win);
+                    char *rightBuff = getcwd(NULL, 0);
+                    strcat(rightBuff, "/");
+                    strcat(rightBuff, rightNamelist[highlightRight - 1]->d_name);
+
+                    if (chdir(rightBuff) == 0) {
+                        while (countRightDir--) {
+                            free(rightNamelist[countRightDir]);
+                        }
+                        free(rightNamelist);
+
+                        countRightDir = scandir(rightBuff, &rightNamelist, NULL, alphasort);
+                        highlightRight = countRightDir;
+
+                        free(rightBuff);
+                    } else {
+                        free(rightBuff);
+                    }
+                    break;
+                }
+            default:
+                refresh();
+                break;
+        }
+        if (flagWindow == 0) {
+            printLeftWindow(left_win, leftNamelist, highlightLeft, countLeftDir);
+        } else {
+            printRightWindow(right_win, rightNamelist, highlightRight, countRightDir);
+        }
+    }
+    memClean(countLeftDir, countRightDir, left_win, leftNamelist, right_win, rightNamelist,
+             curentDir);
+}
+
+void memClean(int countLeftDir, int countRightDir, WINDOW *left_win, struct dirent **leftNamelist,
+              WINDOW *right_win, struct dirent **rightNamelist, char *curentDir) {
+    while (countLeftDir--) {
+        free(leftNamelist[countLeftDir]);
+    }
+    free(leftNamelist);
+
+    while (countRightDir--) {
+        free(rightNamelist[countRightDir]);
+    }
+    free(rightNamelist);
+    free(curentDir);
+    delwin(left_win);
+    delwin(right_win);
+    endwin();
 }
--- simple_mc/function.h
+++ simple_mc/function.h
@@ -13,10 +13,11 @@
 #include <time.h>
 #include <unistd.h>
 
-#define PWD_BUF_SIZE 1024
-
+void mcRun();
 char *printTime(struct stat *st);
 void printLeftWindow(WINDOW *left_win, struct dirent **data, int highlight, int n);
 void printRightWindow(WINDOW *right_win, struct dirent **data, int highlight, int n);
+void memClean(int countLeftDir, int countRightDir, WINDOW *left_win, struct dirent **leftNamelist,
+              WINDOW *right_win, struct dirent **rightNamelist, char *curentDir);
 
 #endif  // _FUNCTION_H
