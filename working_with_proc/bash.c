8ed3dfeb67320d03da6bc2dc8ae8d7f68fc802da
Mahalia Bobbi <mabobbi@si-c8.nsk.21-school.ru>
2022-07-12 15:33:15 +0700

    change

--- working_with_proc/bash.c
+++ working_with_proc/bash.c
@@ -6,22 +6,19 @@ int main(int argc, char *argv[]) {
         pid_t pid;
         int status = 0;
         char *arrayList[argc];
-
         arrayList[argc - 1] = NULL;
 
-        if (argc >= 2) {
-            int j = 0;
-            for (int i = 1; i < argc; ++i) {
-                arrayList[j] = argv[i];
-                ++j;
-            }
+        int j = 0;
+        for (int i = 1; i < argc; ++i) {
+            arrayList[j] = argv[i];
+            ++j;
         }
 
         pid = fork();
         if (pid) {
             pid_t child_pid = 0;
             child_pid = waitpid(-1, &status, 0);
-            printf("\nchild has finish pid = %d\n", child_pid);
+            printf("child has finish pid = %d\n", child_pid);
         } else if (pid == 0) {
             execvp(argv[1], arrayList);
         } else {
