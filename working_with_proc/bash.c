79b8ab8b2e883dad32992a9ad9e9feec0d463d14
Mahalia Bobbi <mabobbi@si-c8.nsk.21-school.ru>
2022-07-12 13:20:16 +0700

    change

--- working_with_proc/bash.c
+++ working_with_proc/bash.c
@@ -2,18 +2,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 int main(int argc, char *argv[]) {
-    char *arrayList[argc];
-    arrayList[argc - 1] = NULL;
+    if (argc > 1) {
+        pid_t pid;
+        int status = 0;
+        char *arrayList[argc];
 
-    if (argc >= 2) {
-        int j = 0;
-        for (int i = 1; i < argc; ++i) {
-            arrayList[j] = argv[i];
-            ++j;
+        arrayList[argc - 1] = NULL;
+
+        if (argc >= 2) {
+            int j = 0;
+            for (int i = 1; i < argc; ++i) {
+                arrayList[j] = argv[i];
+                ++j;
+            }
         }
-    }
 
-    execvp(argv[1], arrayList);
-    free(arrayList);
-    exit(EXIT_SUCCESS);
+        pid = fork();
+        if (pid) {
+            pid_t child_pid = 0;
+            child_pid = waitpid(-1, &status, 0);
+            printf("\nchild has finish pid = %d\n", child_pid);
+        } else if (pid == 0) {
+            execvp(argv[1], arrayList);
+        } else {
+            perror("fork");
+        }
+
+        exit(EXIT_SUCCESS);
+    }
+    printf("not enough arguments\n");
+    return 0;
 }
