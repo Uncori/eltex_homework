95b8c09efe611f3115f458c1d047c9a4701fa7ff
Mahalia Bobbi <mabobbi@si-c8.nsk.21-school.ru>
2022-07-18 11:13:24 +0700

    change

--- flow/main.c
+++ flow/main.c
@@ -23,30 +23,45 @@ static void *fillArrays(void *arg) {
 }
 
 static void *buyArray(void *arg) {
-    int *step = (int *)arg;
+    int step = *((int *)arg);
     int rand_i = rand() % (3 - 0 + 1) + 0;
 
-    while (arrayBuyer[*step] >= 0) {
+    while (arrayBuyer[step] > 0) {
         pthread_mutex_lock(&mutex);
         printf("pthread_self() = %ld\n", pthread_self());
         printf("arrayShop[%d] = %d\n", rand_i, arrayShop[rand_i]);
-        printf("arrayBuyer[%d] = %d\n", *step, arrayBuyer[*step]);
-        if (arrayShop[rand_i] > arrayBuyer[*step]) {
-            arrayShop[rand_i] -= arrayBuyer[*step];
-            arrayBuyer[*step] -= arrayShop[rand_i];
+        printf("arrayBuyer[%d] = %d\n", step, arrayBuyer[step]);
+        if (arrayShop[rand_i] > arrayBuyer[step]) {
+            arrayShop[rand_i] -= arrayBuyer[step];
+            arrayBuyer[step] -= arrayShop[rand_i];
         } else {
-            arrayBuyer[*step] -= arrayShop[rand_i];
+            arrayBuyer[step] -= arrayShop[rand_i];
             arrayShop[rand_i] = 0;
         }
         printf("   arrayShop[%d] = %d\n", rand_i, arrayShop[rand_i]);
-        printf("   arrayBuyer[%d] = %d\n", *step, arrayBuyer[*step]);
+        printf("   arrayBuyer[%d] = %d\n", step, arrayBuyer[step]);
         pthread_mutex_unlock(&mutex);
+        sleep(1);
+    }
+}
+void *fillShop(void *arg) {
+    while (1) {
+        for (int i = 0; i < SHOP_SIZE; i++) {
+            pthread_mutex_lock(&mutex);
+            arrayShop[i] += 500;
+            pthread_mutex_unlock(&mutex);
+            sleep(1);
+        }
     }
 }
 
 int main(void) {
-    pthread_t fillFLow, buyerFlow[BUYERFLOW_COUNT], loaderFlow;
+    pthread_t fillFLow;
+    pthread_t buyerFlow[BUYERFLOW_COUNT];
+    pthread_t loaderFlow;
+
     pthread_mutex_init(&mutex, NULL);
+
     int s;
     s = pthread_create(&fillFLow, NULL, fillArrays, NULL);
     if (s != 0) {
@@ -56,22 +71,38 @@ int main(void) {
     }
 
     s = pthread_join(fillFLow, NULL);
+
     if (s != 0) {
         printf("error join\n");
     } else {
         printf("Thread fillFLow returned\n");
     }
+
     for (int i = 0; i < BUYERFLOW_COUNT; ++i) {
         s = pthread_create(&buyerFlow[i], NULL, buyArray, &i);
     }
     for (int i = 0; i < BUYERFLOW_COUNT; ++i) {
-        s = pthread_join(buyerFlow[0], NULL);
+        s = pthread_join(buyerFlow[i], NULL);
         if (s != 0) {
             printf("error join\n");
         } else {
             printf("Thread buyerFlow[%d] returned\n", i);
         }
     }
+
+    s = pthread_create(&loaderFlow, NULL, fillShop, NULL);
+    if (s != 0) {
+        printf("error create\n");
+    } else {
+        printf("Message from main()\n");
+    }
+    s = pthread_join(loaderFlow, NULL);
+    if (s != 0) {
+        printf("error join\n");
+    } else {
+        printf("Thread loaderFlow returned\n");
+    }
+
     pthread_mutex_destroy(&mutex);
     exit(EXIT_SUCCESS);
 }
