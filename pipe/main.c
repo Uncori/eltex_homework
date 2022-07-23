6b32baffd44f6b21fca5305574fc47bcdf505ac2
Mahalia Bobbi <mabobbi@in-d7.nsk.21-school.ru>
2022-07-23 15:31:30 +0700

    finish

--- pipe/main.c
+++ pipe/main.c
@@ -1,71 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <unistd.h>
-
-typedef struct data_struct {
-  char **arrayParm;
-  int size;
-} Data;
+#include "pipe.h"
 
 int main(int argc, char *argv[]) {
   if (argc > 1) {
-    int countPipe = 0;
+    int countPipe = -1, indexPipe = -1;
+    checkArgv(&countPipe, &indexPipe, argc, argv);
 
-    for (int i = 1; i < argc; ++i) {  // считываем кол-во '|'
-      if (!strncmp(argv[i], "|", 2)) {
-        countPipe += 1;
-      }
+    if (!countPipe && argc != indexPipe + 1) {
+      workWithPipe(indexPipe, argc, argv);
+    } else if (indexPipe && countPipe) {
+      workWithOutPipe(argc, argv);
+    } else {
+      printf("syntax error\n");
+      exit(EXIT_FAILURE);
     }
-
-    int massPipe[countPipe];  // выделяем массив для индексов '|'
-    int j = 0;
-    for (int i = 1; i < argc; ++i) {  // заполняем массив индексами '|'
-      if (!strncmp(argv[i], "|", 2)) {
-        massPipe[j] = i;
-        ++j;
-      }
-    }
-    // выделяем память под аргументы
-    Data *parmDate = (Data *)calloc(countPipe, sizeof(Data));
-    for (int i = 0; i < countPipe; ++i) {
-      parmDate[i].arrayParm = (char **)calloc(1, sizeof(char *));
-      parmDate[i].size = massPipe[i];
-    }
-    for (int i = 0; i < countPipe; ++i) {
-      for (int j = 0; j < massPipe[i]; ++j) {
-        parmDate[i].arrayParm[j] = (char *)calloc(255, sizeof(char));
-      }
-    }
-
-    int k = massPipe[0] + 1;
-    for (int i = 0; i < countPipe; ++i) {
-      for (int j = 0; j < parmDate[i].size; ++j) {
-        for (; k < massPipe[i]; ++k) {
-          parmDate[i].arrayParm[j] = argv[k];
-          printf(" [%d]%s ", k, argv[k]);
-        }
-        printf("\n");
-        printf("j\n");
-      }
-
-      printf("k\n");
-    }
-    /*for (int i = 0; i < countPipe; ++i) {
-      for (int j = 0; j < parmDate[i].size; ++j) {
-        printf("%s\n", parmDate[i].arrayParm[j]);
-      }
-    }*/
-
-    /*char *arrayList[indexPipe];
-    arrayList[indexPipe - 1] = NULL;
-    for (int i = 1; i < indexPipe; ++i) {
-      arrayList[i - 1] = argv[i];
-    }
-    printf("indexPipe = %d\n", indexPipe);
-    for (int i = 0; i < indexPipe; ++i) {
-      printf("%s ", arrayList[i]);
-    }*/
+  } else {
+    printf("not enough arguments\n");
+    exit(EXIT_FAILURE);
   }
+  return 0;
 }
--- pipe/pipe.c
+++ pipe/pipe.c
@@ -1,62 +1,98 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <unistd.h>
+#include "pipe.h"
 
-int main(int argc, char *argv[]) {
-  if (argc > 1) {
-    pid_t pid;
-    int status = 0;
-    char *arrayList[argc];
-    arrayList[argc - 1] = NULL;
-
-    int j = 0;
-    for (int i = 1; i < argc; ++i) {
-      arrayList[j] = argv[i];
-      ++j;
+void checkArgv(int *countPipe, int *indexPipe, int argc, char *argv[]) {
+  for (int i = 1; i < argc && *countPipe; ++i) {
+    if (!strncmp(argv[i], "|", 2)) {
+      *countPipe += 1;
+      *indexPipe = i;
     }
+  }
+}
 
-    pid = fork();
-    if (pid) {
-      pid = waitpid(-1, &status, 0);
-    } else if (pid == 0) {
-      execvp(argv[1], arrayList);
-    } else {
-      perror("fork");
-    }
-
-    exit(EXIT_SUCCESS);
-  } else {
-    exit(EXIT_FAILURE);
-    printf("not enough arguments\n");
+void allocateAraay(const int *indexPipe, int argc, char *firstArr[],
+                   char *secondArr[], char *argv[]) {
+  firstArr[*indexPipe - 1] = NULL;
+  secondArr[argc - *indexPipe - 1] = NULL;
+  for (int i = 1; i < *indexPipe; ++i) {
+    firstArr[i - 1] = argv[i];
   }
 
-  return 0;
+  int j = 0;
+  for (int i = *indexPipe + 1; i < argc; ++i) {
+    secondArr[j] = argv[i];
+    ++j;
+  }
 }
 
-int pipeFd[2];
-pipe(pipeFd);
-int pid1, pid2;
-pid1 = fork();
-if (pid1 == 0) {
-  dup2(pipeFd[1], STDOUT_FILENO);
-  close(pipeFd[0]);
-  close(pipeFd[1]);
-  execlp("ls", "ls", "-la", NULL);
-}
-pid2 = fork();
-if (pid2 == 0) {
-  dup2(pipeFd[0], STDIN_FILENO);
-  close(pipeFd[0]);
-  close(pipeFd[1]);
-  execlp("grep", "grep", "a.", NULL);
+void checkRes(const int *res) {
+  if (*res == -1) exit(EXIT_FAILURE);
 }
 
-close(pipeFd[0]);
-close(pipeFd[1]);
+void workWithPipe(int indexPipe, int argc, char *argv[]) {
+  char *firstArr[indexPipe];
+  char *secondArr[argc - indexPipe];
+  allocateAraay(&indexPipe, argc, firstArr, secondArr, argv);
 
-waitpid(pid1, NULL, 0);
-waitpid(pid2, NULL, 0);
+  int pipeFd[2], res = 0, pid1 = 0, pid2 = 0;
 
-exit(EXIT_SUCCESS);
+  res = pipe(pipeFd);
+  checkRes(&res);
+
+  pid1 = fork();
+  checkRes(&pid1);
+
+  if (!pid1) {
+    res = dup2(pipeFd[1], STDOUT_FILENO);
+    checkRes(&res);
+    res = close(pipeFd[0]);
+    checkRes(&res);
+    res = close(pipeFd[1]);
+    checkRes(&res);
+    res = execvp(firstArr[0], firstArr);
+    checkRes(&res);
+  }
+
+  pid2 = fork();
+  checkRes(&pid1);
+
+  if (!pid2) {
+    res = dup2(pipeFd[0], STDIN_FILENO);
+    checkRes(&res);
+    res = close(pipeFd[0]);
+    checkRes(&res);
+    res = close(pipeFd[1]);
+    checkRes(&res);
+    res = execvp(secondArr[0], secondArr);
+    checkRes(&res);
+  }
+
+  res = close(pipeFd[0]);
+  checkRes(&res);
+  res = close(pipeFd[1]);
+  checkRes(&res);
+
+  waitpid(pid1, NULL, 0);
+  waitpid(pid2, NULL, 0);
+
+  exit(EXIT_SUCCESS);
+}
+
+void workWithOutPipe(int argc, char *argv[]) {
+  pid_t pid, res = 0;
+  char *arrayList[argc];
+  arrayList[argc - 1] = NULL;
+
+  for (int i = 1; i < argc; ++i) {
+    arrayList[i - 1] = argv[i];
+  }
+
+  pid = fork();
+  checkRes(&pid);
+  waitpid(-1, NULL, 0);
+  if (!pid) {
+    res = execvp(argv[1], arrayList);
+    checkRes(&res);
+  }
+
+  exit(EXIT_SUCCESS);
+}
