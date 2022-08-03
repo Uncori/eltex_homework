9010e8f1ba001d0f858acb016aea80d108afa76b
Mahalia Bobbi <mabobbi@si-c8.nsk.21-school.ru>
2022-08-03 17:19:56 +0700

    added

---
+++ message_queue/mqueue.c
@@ -0,0 +1,12 @@
+#include <mqueue.h>
+
+int main(int argc, char *argv[]) {
+  mqd_t mqd;
+  struct mq_attr attr;
+  mqd = mq_open(argv[1], O_RDONLY);
+  mq_getattr(mqd, &attr);
+  printf("Maximum # of messages on queue: %ld\n", attr.mq_maxmsg);
+  printf("Maximum message size: %ld\n", attr.mq_msgsize);
+  printf("# of messages currently on queue: %ld\n", attr.mq_curmsgs);
+  exit(EXIT_SUCCESS);
+}
