6fbe5a3b1e6dcdcbc8f5bcd485986d156f3557b6
Mahalia Bobbi <mabobbi@si-c8.nsk.21-school.ru>
2022-08-04 11:01:21 +0700

    change

--- message_queue/mqueue.c
+++ message_queue/mqueue.c
@@ -1,14 +1,6 @@
+#include <fcntl.h>
 #include <mqueue.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <sys/stat.h>
 
-int main(int argc, char *argv[]) {
-  mqd_t mqd;
-  struct mq_attr attr;
-  mqd = mq_open(argv[1], O_RDONLY);
-  mq_getattr(mqd, &attr);
-  printf("Maximum # of messages on queue: %ld\n", attr.mq_maxmsg);
-  printf("Maximum message size: %ld\n", attr.mq_msgsize);
-  printf("# of messages currently on queue: %ld\n", attr.mq_curmsgs);
-  exit(EXIT_SUCCESS);
-}
+int main() { printf("Privet!\n"); }
