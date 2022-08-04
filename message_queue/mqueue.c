e5d310469b37c75a2e4fd2a868e481efe3778546
mabobbi <chernenko00@inbox.ru>
2022-08-04 05:01:57 +0000

    change

--- message_queue/mqueue.c
+++ message_queue/mqueue.c
@@ -3,4 +3,11 @@
 #include <stdio.h>
 #include <sys/stat.h>
 
-int main() { printf("Privet!\n"); }
+int main() {
+int flags = O_RDWR | O_CREAT;
+mqd_t mqd;
+mqd = mq_open("/server_chat", flags, 0777,NULL);
+mq_close(mqd);
+exit(EXIT_SUCSES);
+
+}
