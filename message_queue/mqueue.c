72aba95c6651446e2bee5683adada7202a4350bb
mabobbi <chernenko00@inbox.ru>
2022-08-04 05:31:32 +0000

    change

--- message_queue/mqueue.c
+++ message_queue/mqueue.c
@@ -2,12 +2,27 @@
 #include <mqueue.h>
 #include <stdio.h>
 #include <sys/stat.h>
+#include <stdlib.h>
+
 
 int main() {
-int flags = O_RDWR | O_CREAT;
-mqd_t mqd;
-mqd = mq_open("/server_chat", flags, 0777,NULL);
-mq_close(mqd);
-exit(EXIT_SUCSES);
-
+  mqd_t mqd;
+  struct mq_attr attr;
+  mqd = mq_open("/tmp/test", O_RDONLY);
+  mq_getattr(mqd, &attr);
+  printf(
+      "max #msgs = %ld, max #bytes/msg = %ld,"
+      "#currently on queue = %ld\n",
+      attr.mq_maxmsg, attr.mq_msgsize, attr.mq_curmsgs);
+  mq_close(mqd);
+  exit(0);
 }
+
+
+
+
+
+
+
+
+
