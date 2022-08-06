35da23033e8b97e0d196165281f32e25dd58bab2
Mahalia Bobbi <mabobbi@in-c1.nsk.21-school.ru>
2022-08-06 20:23:59 +0700

    change style

--- message_queue/client.c
+++ message_queue/client.c
@@ -1,5 +1,3 @@
-
-
 #include <fcntl.h>
 #include <mqueue.h>
 #include <stdio.h>
@@ -22,32 +20,28 @@ int main() {
   mqd = mq_open(NAME, flags);
 
   if (mqd) {
-    printf("Server[%d] create \"%s\"\n",mqd, NAME);
+    printf("Server[%d] create \"%s\"\n", mqd, NAME);
   }
 
   if (!mq_getattr(mqd, &attr)) {
     printf("mq_maxmsg = %ld, mq_msgsize = %ld, mq_curmsgs = %ld\n",
            attr.mq_maxmsg, attr.mq_msgsize, attr.mq_curmsgs);
   }
-  
+
   len = attr.mq_msgsize;
 
-  while(1){
-  msgBuffer = calloc(len, sizeof(char));
-  fgets(msgBuffer,len,stdin);
-  if (!mq_send(mqd, msgBuffer, strlen(msgBuffer), 0)) {
-    printf("Message sent = \"%s\"\n", msgBuffer);
-    free(msgBuffer);
-  } else {
-    printf("Message not sent! = \"%s\"\n", msgBuffer);
-    free(msgBuffer);
+  while (1) {
+    msgBuffer = calloc(len, sizeof(char));
+    fgets(msgBuffer, len, stdin);
+    if (!mq_send(mqd, msgBuffer, strlen(msgBuffer), 0)) {
+      printf("Message sent = \"%s\"\n", msgBuffer);
+      free(msgBuffer);
+    } else {
+      printf("Message not sent! = \"%s\"\n", msgBuffer);
+      free(msgBuffer);
+    }
   }
-  }
 
-
-  
-
- 
   if (!mq_close(mqd)) {
     printf("CLOSE!\n");
   }
--- message_queue/server.c
+++ message_queue/server.c
@@ -18,40 +18,40 @@ int main() {
   struct mq_attr attr;
 
   flags = O_RDONLY | O_CREAT;
-  
+
   mqd = mq_open(NAME, flags, 0777, NULL);
 
   if (mqd) {
     printf("Server[%d] start \"%s\"\n", mqd, NAME);
   }
-  
+
   if (!mq_getattr(mqd, &attr)) {
-    printf("\tmax count message = %ld\n\tmax messagesize = %ld\n\tcurmsgs = %ld\n",
-           attr.mq_maxmsg, attr.mq_msgsize, attr.mq_curmsgs);
+    printf(
+        "\tmax count message = %ld\n\tmax messagesize = %ld\n\tcurmsgs = %ld\n",
+        attr.mq_maxmsg, attr.mq_msgsize, attr.mq_curmsgs);
   }
 
   long len = 0;
   len = attr.mq_msgsize;
 
-  while(1){
-  buffer = calloc(len, sizeof(char));
+  while (1) {
+    buffer = calloc(len, sizeof(char));
 
-  numRead = mq_receive(mqd, buffer, len, &prio);
-  if(numRead != -1){
-  printf("Read %ld bytes; priority = %u\n", (long)numRead, prio);
-  printf("%s",(char*)buffer);
-  free(buffer);
-  }
+    numRead = mq_receive(mqd, buffer, len, &prio);
+    if (numRead != -1) {
+      printf("Read %ld bytes; priority = %u\n", (long)numRead, prio);
+      printf("%s", (char *)buffer);
+      free(buffer);
+    }
   }
 
   if (!mq_close(mqd)) {
     printf("CLOSE!\n");
   }
 
-  if(!mq_unlink(NAME)) {
+  if (!mq_unlink(NAME)) {
     printf("UNLINK!\n");
   }
   free(buffer);
   exit(0);
 }
-
