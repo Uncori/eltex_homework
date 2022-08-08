406a21b8cbef75b98b825d6c30dbb00b46e64b8e
Uncori <chernenko00@inbox.ru>
2022-08-08 16:31:21 +0700

    refresh

---
+++ 4.Directory/functions.h
@@ -0,0 +1,33 @@
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct subscriber {
+    char *second_name;
+    char *first_name;
+    char *last_name;
+    char *phone_number;
+} subscriber;
+
+typedef struct directory {
+    unsigned int count_subscriber;
+    subscriber *subscriber;
+} directory;
+
+void memory_allocation_directory(directory *data);
+void remove_directory(directory *data);
+void print_subscriber(directory *data);
+void print_subscriber_i(directory *data, unsigned int *i);
+void second_menu(directory *data);
+void second_find(directory *data);
+void first_find(directory *data);
+void last_find(directory *data);
+void phone_find(directory *data);
+void input_directory(directory *data);
+void save_directory(directory *data);
+void read_directory(directory *data);
+
+#endif  // _FUNCTIONS_H
---
+++ 4.Directory/main.c
@@ -0,0 +1,6 @@
+#include "functions.h"
+
+int main(void) {
+    directory data;
+    input_directory(&data);
+}
