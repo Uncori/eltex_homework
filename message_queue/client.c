ecbd9fb04b0e3a4ddf14b5841a98412235a3f2e8
Uncori <chernenko00@inbox.ru>
2022-08-08 12:27:50 +0700

    change

--- message_queue/client.c
+++ message_queue/client.c
@@ -7,12 +7,20 @@
 
 #define NAME "/server"
 
+
+void checkRes(const int *res) {
+  if (*res == -1) exit(EXIT_FAILURE);
+}
+
 int main() {
-  int flags = 0;
+  int flags = 0, res = 0;
   mqd_t mqd;
   long len = 0;
 
   char *msgBuffer;
+  char *nicName;
+  char *outputMsg;
+
   struct mq_attr attr;
 
   flags = O_WRONLY;
@@ -20,32 +28,60 @@ int main() {
   mqd = mq_open(NAME, flags);
 
   if (mqd) {
-    printf("Server[%d] create \"%s\"\n", mqd, NAME);
-  }
 
-  if (!mq_getattr(mqd, &attr)) {
-    printf("mq_maxmsg = %ld, mq_msgsize = %ld, mq_curmsgs = %ld\n",
+  nicName = calloc(255,sizeof(char));
+  printf("Write your nickname: ");
+  fgets(nicName, 255, stdin);
+
+  res = mq_send(mqd,"[client connect]\n", 18, 0);
+  checkRes(&res);
+
+  printf("Hello, %s\tServer[%d] connect \"%s\"\n",nicName, mqd, NAME);
+  res = mq_getattr(mqd, &attr);
+
+  checkRes(&res);
+  printf("\tmq_maxmsg = %ld\tmq_msgsize = %ld\tmq_curmsgs = %ld\n",
            attr.mq_maxmsg, attr.mq_msgsize, attr.mq_curmsgs);
-  }
+
+  printf("\tto exit print 'exit'\n");
 
   len = attr.mq_msgsize;
 
   while (1) {
-    msgBuffer = calloc(len, sizeof(char));
+    msgBuffer = calloc(255, sizeof(char));
+    outputMsg = calloc(len,sizeof(char));
     fgets(msgBuffer, len, stdin);
-    if (!mq_send(mqd, msgBuffer, strlen(msgBuffer), 0)) {
-      printf("Message sent = \"%s\"\n", msgBuffer);
+    strncat(outputMsg, nicName, strlen(nicName) - 1);
+    strncat(outputMsg, ": ", 3);
+    strncat(outputMsg, msgBuffer, strlen(msgBuffer));
+
+    if(!strncmp(msgBuffer,"exit\n",6)){
+      free(msgBuffer);
+      msgBuffer = calloc(255, sizeof(char));
+      strncat(msgBuffer, "[client disconnect]\n", 21);
+      mq_send(mqd, msgBuffer, strlen(msgBuffer), 0);
+      break;
+    }
+    if (!mq_send(mqd, outputMsg, strlen(outputMsg), 0)) {
+      printf("[message sent]\n");
+      free(outputMsg);
       free(msgBuffer);
     } else {
-      printf("Message not sent! = \"%s\"\n", msgBuffer);
+      printf("[message not sent!]\n");
+      free(outputMsg);
       free(msgBuffer);
     }
   }
 
-  if (!mq_close(mqd)) {
-    printf("CLOSE!\n");
-  }
-
+  res = mq_close(mqd);
+  checkRes(&res);
+  printf("Good bye!\n");
+  
+  free(outputMsg);
+  free(nicName);
   free(msgBuffer);
+  } else {
+    printf("No connection to server!\n");
+  }
   exit(0);
 }
--- message_queue/server.c
+++ message_queue/server.c
@@ -5,10 +5,16 @@
 #include <string.h>
 #include <sys/stat.h>
 
-#define NAME "/server"
+#define SERV_NAME "/server"
+
+
+void checkRes(const int *res) {
+  if (*res == -1) exit(EXIT_FAILURE);
+}
 
 int main() {
-  int flags = 0;
+  int flags = 0, res = 0;
+  int countClient = 0, offServ = 1;
   mqd_t mqd;
 
   unsigned int prio;
@@ -19,39 +25,46 @@ int main() {
 
   flags = O_RDONLY | O_CREAT;
 
-  mqd = mq_open(NAME, flags, 0777, NULL);
+  mqd = mq_open(SERV_NAME, flags, 0777, NULL);
 
   if (mqd) {
-    printf("Server[%d] start \"%s\"\n", mqd, NAME);
+    printf("Server[%d] start \"%s\"\n", mqd, SERV_NAME);
   }
-
-  if (!mq_getattr(mqd, &attr)) {
-    printf(
+  res = mq_getattr(mqd, &attr);
+  checkRes(&res);
+  printf(
         "\tmax count message = %ld\n\tmax messagesize = %ld\n\tcurmsgs = %ld\n",
         attr.mq_maxmsg, attr.mq_msgsize, attr.mq_curmsgs);
-  }
 
   long len = 0;
   len = attr.mq_msgsize;
 
-  while (1) {
+  while (offServ) {
     buffer = calloc(len, sizeof(char));
-
     numRead = mq_receive(mqd, buffer, len, &prio);
     if (numRead != -1) {
-      printf("Read %ld bytes; priority = %u\n", (long)numRead, prio);
+       if(!strncmp((char*)buffer,"[client connect]\n",18)) {
+        ++countClient;
+    }
+    if(!strncmp((char*)buffer,"[client disconnect]\n",21)) {
+        --countClient;
+    }
       printf("%s", (char *)buffer);
       free(buffer);
+    } 
+    if(!countClient){
+      offServ = 0;
     }
   }
 
-  if (!mq_close(mqd)) {
-    printf("CLOSE!\n");
-  }
+  res = mq_close(mqd);
+  checkRes(&res);
+  printf("serv [%d] \"%s\" close!\n", mqd, SERV_NAME);
 
-  if (!mq_unlink(NAME)) {
-    printf("UNLINK!\n");
-  }
-  free(buffer);
+
+  res = mq_unlink(SERV_NAME);
+  checkRes(&res);
+  printf("serv [%d] \"%s\" unlink!\n", mqd, SERV_NAME);
+  
   exit(0);
 }
